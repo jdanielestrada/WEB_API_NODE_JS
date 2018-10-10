9357ad9e1b59ae91d33e2d99f516241b4ec0a5e3
Jose Daniel Estrada <jdanielestrada18@gmail.com>
2018-10-10 17:21:45 -0500

    agregar insumos al producto

--- routes/cotizaciones/gestion_cotizaciones.js
+++ routes/cotizaciones/gestion_cotizaciones.js
@@ -83,7 +83,7 @@ router.get('/get_productos_desarrollados', function (req, res, next) {
         // ... error checks
         if (err) {
             console.error(err);
-          return res.json(err);
+            return res.json(err);
         }
 
         // Stored Procedure
@@ -1232,7 +1232,7 @@ router.get('/get_productos_desarrollados_for_gestion_imagen', function (req, res
 
         // Stored Procedure
         var request = new sql.Request(connection);
-        
+
         request.execute('RTA.SSP_GET_PRODUCTOS_DESARROLLADOS_FOR_GESTION_IMAGEN', function (err, recordsets, returnValue) {
             if (err) {
                 res.json(err);
@@ -1247,7 +1247,7 @@ router.get('/get_productos_desarrollados_for_gestion_imagen', function (req, res
 });
 
 router.post('/almacenar_imagen_producto', multipartMiddleware, function (req, res) {
-    
+
     var tamaño = 0;
     var files;
     if (req.files.file != undefined) {
@@ -1263,7 +1263,7 @@ router.post('/almacenar_imagen_producto', multipartMiddleware, function (req, re
 
             var ext = "png";// (item.name).split('.');
             //ext = ext[ext.length - 1];
-     
+
             var nombre_archivo = req.body.ID_ITEM;
             var rutaFisicaServidor = config.pathBaseGestionDocumental + config.rutaImgProductos;
 
@@ -1275,7 +1275,7 @@ router.post('/almacenar_imagen_producto', multipartMiddleware, function (req, re
                         var imageName = item.name;
                         /// If there's an error
                         if (!imageName) {
-                            
+
                             res.end();
                         } else {
 
@@ -1284,16 +1284,16 @@ router.post('/almacenar_imagen_producto', multipartMiddleware, function (req, re
                             /*escribimos los archivos en la ruta indicada*/
                             fs.writeFile(newPath, data, function (err) {
                                 if (err) {
-                                    
+
                                     res.json({
                                         error: err,
                                         MSG: err.message
                                     });
-                          
+
                                 } else {
 
                                     contador--;
-                                    
+
                                     if (contador == 0) {
 
                                         res.json({
@@ -1312,7 +1312,7 @@ router.post('/almacenar_imagen_producto', multipartMiddleware, function (req, re
                 }
             });
         });
-    } 
+    }
 });
 
 router.get('/get_all_materiales_productos_desarrollados', function (req, res, next) {
@@ -1328,7 +1328,7 @@ router.get('/get_all_materiales_productos_desarrollados', function (req, res, ne
 
         // Stored Procedure
         var request = new sql.Request(connection);
-        
+
         request.execute('RTA.SSP_GET_ALL_MATERIALES_PRODUCTOS_DESARROLLADOS', function (err, recordsets, returnValue) {
             if (err) {
                 res.json(err);
@@ -1549,7 +1549,7 @@ router.post('/insert_nuevo_producto', function (req, res, next) {
         request.input("IN_ID_CRICLA2", sql.VarChar, req.body.datos_item.ID_CRICLA2);
         request.input("IN_ID_CRICLA3", sql.VarChar, req.body.datos_item.ID_CRICLA3);
         request.input("IN_ID_CRICLA4", sql.VarChar, req.body.datos_item.ID_CRICLA4);
-        request.input("IN_ESTADO", sql.VarChar, req.body.datos_item.ESTADO);        
+        request.input("IN_ESTADO", sql.VarChar, req.body.datos_item.ESTADO);
         request.input("IN_UNIMED_INV_1", sql.VarChar, req.body.datos_item.UNIMED_INV_1);
         request.input("IN_UNIMED_INV_2", sql.VarChar, req.body.datos_item.UNIMED_INV_2);
         request.input("IN_FACTOR_INV_2", sql.Decimal(20, 4), req.body.datos_item.FACTOR_INV_2);
@@ -1558,7 +1558,7 @@ router.post('/insert_nuevo_producto', function (req, res, next) {
         request.input("IN_PESO", sql.Decimal(20, 4), req.body.datos_item.PESO);
         request.input("IN_VOLUMEN", sql.Decimal(20, 4), req.body.datos_item.VOLUMEN);
         request.input("IN_ID_CURVA", sql.VarChar, req.body.datos_item.ID_CURVA);
-        request.input("IN_IMPUESTO", sql.VarChar, req.body.datos_item.IMPUESTO);        
+        request.input("IN_IMPUESTO", sql.VarChar, req.body.datos_item.IMPUESTO);
         request.input("IN_RTEFTE", sql.VarChar, req.body.datos_item.RTEFTE);
         request.input("IN_IND_CLASIF", sql.VarChar, req.body.datos_item.IND_CLASIF);
         request.input("IN_ID_BODEGA_DEFAULT", sql.VarChar, req.body.datos_item.ID_BODEGA_DEFAULT);
@@ -1566,7 +1566,7 @@ router.post('/insert_nuevo_producto', function (req, res, next) {
         request.input("IN_COSTO_EST_ACUM", sql.Decimal(20, 4), req.body.datos_item.COSTO_EST_ACUM);
         request.input("IN_COSTO_ACT_ESTENIV", sql.Decimal(20, 4), req.body.datos_item.COSTO_ACT_ESTENIV);
         request.input("IN_COSTO_ACT_ACUM", sql.Decimal(20, 4), req.body.datos_item.COSTO_ACT_ACUM);
-        request.input("IN_ULTIMO_COSTO_ED", sql.Decimal(20, 4), req.body.datos_item.ULTIMO_COSTO_ED);        
+        request.input("IN_ULTIMO_COSTO_ED", sql.Decimal(20, 4), req.body.datos_item.ULTIMO_COSTO_ED);
         request.input("IN_ID_TERC", sql.VarChar, req.body.datos_item.ID_TERC);
         request.input("IN_NOM_TERC", sql.VarChar, req.body.datos_item.NOM_TERC);
         request.input("IN_DESC_ITEM_PADRE", sql.VarChar, req.body.datos_item.DESC_ITEM_PADRE);
@@ -1577,7 +1577,7 @@ router.post('/insert_nuevo_producto', function (req, res, next) {
         request.input("IN_ID_RTE_RENTA", sql.VarChar, req.body.datos_item.ID_RTE_RENTA);
         request.input("IN_ID_BASE_RENTA", sql.VarChar, req.body.datos_item.ID_BASE_RENTA);
         request.input("IN_ID_PLAN_MAESTRO", sql.VarChar, req.body.datos_item.ID_PLAN_MAESTRO);
-        request.input("IN_ID_EXIGE_OP", sql.VarChar, req.body.datos_item.ID_EXIGE_OP);        
+        request.input("IN_ID_EXIGE_OP", sql.VarChar, req.body.datos_item.ID_EXIGE_OP);
         request.input("IN_ID_POLIT_ORDEN", sql.VarChar, req.body.datos_item.ID_POLIT_ORDEN);
         request.input("IN_TAM_PROM_LOTE", sql.Decimal(20, 4), req.body.datos_item.TAM_PROM_LOTE);
         request.input("IN_TIEMPO_SEG", sql.VarChar, req.body.datos_item.TIEMPO_SEG);
@@ -1589,7 +1589,7 @@ router.post('/insert_nuevo_producto', function (req, res, next) {
         request.input("IN_MIN_ORDENAR", sql.Decimal(20, 4), req.body.datos_item.MIN_ORDENAR);
         request.input("IN_MAX_ORDENAR", sql.Decimal(20, 4), req.body.datos_item.MAX_ORDENAR);
         request.input("IN_INCREM_ORDENAR", sql.Decimal(20, 4), req.body.datos_item.INCREM_ORDENAR);
-        request.input("IN_PORC_DESPER", sql.Decimal(20, 4), req.body.datos_item.PORC_DESPER);        
+        request.input("IN_PORC_DESPER", sql.Decimal(20, 4), req.body.datos_item.PORC_DESPER);
         request.input("IN_COD_RUTA", sql.VarChar, req.body.datos_item.COD_RUTA);
         request.input("IN_COD_LISMAT", sql.VarChar, req.body.datos_item.COD_LISMAT);
         request.input("IN_EXT_DEFAULT", sql.VarChar, req.body.datos_item.EXT_DEFAULT);
@@ -1622,16 +1622,105 @@ router.post('/insert_nuevo_producto', function (req, res, next) {
 
                     });
                 } else {
-                    /*hacemos commit*/
-                    transaction.commit(function (err, recordset) {
-                        // ... error checks
-                        res.json({
-                            data: [],
-                            'MSG': request.parameters.MSG.value
-                        });
 
-                        console.log("Transaction commited.");
-                    });
+                    var cantRegistrosInsumos = Object.keys(req.body.insumos_producto).length;
+
+                    /* almacenamos las asignaciones a los operarios del corte para la op seleccionada */
+                    async.each(req.body.insumos_producto, function (item, callback) {
+
+                        var requestInsumo = new sql.Request(transaction);
+
+                        requestInsumo.verbose = false;
+                        
+                        requestInsumo.input("IN_ID_COD_ITEM_P", sql.VarChar, req.body.datos_item.ID_ITEM);
+                        requestInsumo.input("IN_ID_EXT_ITEM_P", sql.VarChar, item.ID_EXT_ITEM_P);
+                        requestInsumo.input("IN_ID_UNIMED_P", sql.VarChar, item.ID_UNIMED_P);
+                        requestInsumo.input("IN_ID_LISTA", sql.VarChar, item.ID_LISTA);
+                        requestInsumo.input("IN_CONSECUTIVO", sql.VarChar, item.CONSECUTIVO);
+                        requestInsumo.input("IN_ID_COD_ITEM_C", sql.VarChar, item.ID_COD_ITEM_C);
+                        requestInsumo.input("IN_ID_EXT_ITEM_C", sql.VarChar, item.ID_EXT_ITEM_C);
+                        requestInsumo.input("IN_ID_UNIMED_C", sql.VarChar, item.ID_UNIMED_C);
+                        requestInsumo.input("IN_CANTIDAD", sql.Decimal(20, 5), item.CANTIDAD);
+                        requestInsumo.input("IN_CANTIDAD_BASE", sql.Decimal(20, 5), item.CANTIDAD_BASE);
+                        requestInsumo.input("IN_CANTIDAD_REQUERIDA", sql.Decimal(20, 5), item.CANTIDAD_REQUERIDA);
+                        requestInsumo.input("IN_PESO", sql.Decimal(20, 5), item.PESO);
+                        requestInsumo.input("IN_VOLUMEN", sql.Decimal(20, 5), item.VOLUMEN);
+                        requestInsumo.input("IN_FECHA_INI", sql.VarChar, item.FECHA_INI);
+                        requestInsumo.input("IN_FECHA_FIN", sql.VarChar, item.FECHA_FIN);
+                        requestInsumo.input("IN_ID_GRUPO_CONSUMO", sql.VarChar, item.ID_GRUPO_CONSUMO);
+                        requestInsumo.input("IN_ID_OPERACION", sql.VarChar, item.ID_OPERACION);
+                        requestInsumo.input("IN_SOLO_COSTEO", sql.VarChar, item.SOLO_COSTEO);
+                        requestInsumo.input("IN_PORC_DESPERDICIO", sql.Decimal(20, 5), item.PORC_DESPERDICIO);
+                        requestInsumo.input("IN_TIEMPO_REPOSICION", sql.Decimal(20, 5), item.TIEMPO_REPOSICION);
+                        requestInsumo.input("IN_OBSERVACION", sql.VarChar, item.OBSERVACION);
+                        requestInsumo.input("IN_BODEGA_CONSUMO", sql.VarChar, item.BODEGA_CONSUMO);
+                        requestInsumo.input("IN_PORC_CONSUMO", sql.Decimal(20, 5), item.PORC_CONSUMO);
+                        requestInsumo.input("IN_USOS_TALLA1", sql.VarChar, item.USOS_TALLA1);
+                        requestInsumo.input("IN_USOS_TALLA2", sql.VarChar, item.USOS_TALLA2);
+                        requestInsumo.input("IN_USOS_TALLA3", sql.VarChar, item.USOS_TALLA3);
+                        requestInsumo.input("IN_USOS_TALLA4", sql.VarChar, item.USOS_TALLA4);
+                        requestInsumo.input("IN_USOS_TALLA5", sql.VarChar, item.USOS_TALLA5);
+                        requestInsumo.input("IN_USOS_TALLA6", sql.VarChar, item.USOS_TALLA6);
+                        requestInsumo.input("IN_USOS_TALLA7", sql.VarChar, item.USOS_TALLA7);
+                        requestInsumo.input("IN_USOS_TALLA8", sql.VarChar, item.USOS_TALLA8);
+                        requestInsumo.input("IN_USOS_TALLA9", sql.VarChar, item.USOS_TALLA9);
+                        requestInsumo.input("IN_USOS_TALLA10", sql.VarChar, item.USOS_TALLA10);
+                        requestInsumo.input("IN_COSTO_EST_MP", sql.Decimal(20, 5), item.COSTO_EST_MP);
+                        requestInsumo.input("IN_COSTO_ACT_MP", sql.Decimal(20, 5), item.COSTO_ACT_MP);
+                        
+                        requestInsumo.output("MSG", sql.VarChar);
+
+                        requestInsumo.execute('RTA.SSP_INSERT_INSUMO_NUEVO_PRODUCTO', function (err, recordsets, returnValue) {
+                            if (err) {
+                                // ... error checks
+                                callback(err.message);
+
+                            } else if (requestInsumo.parameters.MSG.value !== "OK") {
+                                callback(requestInsumo.parameters.MSG.value);
+                            } else {
+                                cantRegistrosInsumos--;
+                                callback();
+                            }
+
+                        });
+                    },
+                        function (err) {
+
+                            if (err) {
+                                transaction.rollback(function (err2) {
+                                });
+                                return res.json({
+                                    error: "err",
+                                    MSG: err
+                                });
+
+                            } else {
+
+                                if (cantRegistrosInsumos === 0) {
+                                    
+                                    transaction.commit(function (err, recordset) {
+                                        res.json({
+                                            data: [],
+                                            MSG: "OK"
+                                        });
+                                        console.log("Transaction commited.");
+                                    });
+                                }
+                            }
+                        });
+                    
+                    /*hacemos commit*/
+                    //transaction.commit(function (err, recordset) {
+                    //    // ... error checks
+                    //    res.json({
+                    //        data: [],
+                    //        'MSG': request.parameters.MSG.value
+                    //    });
+
+                    //    console.log("Transaction commited.");
+                    //});
+
+
                 }
             }
         });
